cdc80fba356fca3d5b5384be4bdf5d4a2b39189f
Mario Winter <mariowinter.sg@gmail.com>
2024-06-21 23:25:11 +0200

    About Me component added

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
     selector: "app-landingpage",
     standalone: true,
-    imports: [HeroSectionComponent],
+    imports: [HeroSectionComponent, AboutMeComponent],
     templateUrl: "./landingpage.component.html",
     styleUrl: "./landingpage.component.scss",
 })
