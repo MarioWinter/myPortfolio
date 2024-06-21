26f6def195ac5cd71dfeeb0b3c7514ea78ee2eb6
Mario Winter <mariowinter.sg@gmail.com>
2024-06-21 12:36:05 +0200

    Scrolldown-Animation für den Heldenbereich verbessert

--- src/app/landingpage/hero-section/hero-section.component.ts
+++ src/app/landingpage/hero-section/hero-section.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-hero-section',
-  standalone: true,
-  imports: [],
-  templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+    selector: "app-hero-section",
+    standalone: true,
+    imports: [],
+    templateUrl: "./hero-section.component.html",
+    styleUrl: "./hero-section.component.scss",
 })
 export class HeroSectionComponent {
-
+    emailAddress = "mariowinter.sg@gmail.com";
 }
