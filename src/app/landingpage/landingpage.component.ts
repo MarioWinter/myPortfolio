bc43dac1391b539e69a72e0fe8b1f4edb11468c9
Mario Winter <mariowinter.sg@gmail.com>
2024-06-23 23:18:21 +0200

    my Skills component frame added

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ArrowLeftComponent } from "./arrow-left/arrow-left.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
 
 @Component({
     selector: "app-landingpage",
     standalone: true,
-    imports: [CommonModule, HeroSectionComponent, AboutMeComponent, ArrowLeftComponent],
+    imports: [CommonModule, HeroSectionComponent, AboutMeComponent, ArrowLeftComponent, MySkillsComponent],
     templateUrl: "./landingpage.component.html",
     styleUrl: "./landingpage.component.scss",
 })
