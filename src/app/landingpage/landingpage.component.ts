f7643100612f8a5050c22334aefc1d32f079caf1
Mario Winter <mariowinter.sg@gmail.com>
2024-06-26 22:49:22 +0200

    Portfolio component created

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -5,11 +5,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { ArrowLeftComponent } from "./arrow-left/arrow-left.component";
 import { ArrowRightComponent } from "./arrow-right/arrow-right.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
     selector: "app-landingpage",
     standalone: true,
-    imports: [CommonModule, HeroSectionComponent, AboutMeComponent, ArrowLeftComponent, ArrowRightComponent, MySkillsComponent],
+    imports: [CommonModule, HeroSectionComponent, AboutMeComponent, ArrowLeftComponent, ArrowRightComponent, MySkillsComponent, PortfolioComponent],
     templateUrl: "./landingpage.component.html",
     styleUrl: "./landingpage.component.scss",
 })
