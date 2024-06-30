c637fb58687961124a26bd39dbacc9ce16e05a0f
Mario Winter <mariowinter.sg@gmail.com>
2024-06-30 14:25:09 +0200

    Dynamic styling project component added

--- src/app/landingpage/portfolio/project/project.component.ts
+++ src/app/landingpage/portfolio/project/project.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "app-project",
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: "./project.component.html",
     styleUrl: "./project.component.scss",
 })
 export class projectComponent {
     @Input() name: string = "";
     @Input() stack: string = "";
+    @Input() img: string = "";
     @Input() description: string = "";
     @Input() github: string = "";
     @Input() allProjects: number = 0;
     @Input() projectNr: number = 0;
+    @Input() isRight: boolean = true;
+
+    constructor() {
+        console.log(this.isRight);
+    }
 }
--- src/app/shared/components/myproject-list.service.ts
+++ src/app/shared/components/myproject-list.service.ts
@@ -9,24 +9,28 @@ export class MyprojectListService {
         {
             name: "Portfolio Website",
             stack: ["Angular", "TypeScript", "SCSS", "HTML"],
+            img: "el-pollo-loco.png",
             description: "Eine persönliche Portfolio-Website zur Präsentation meiner Projekte und Fähigkeiten.",
             github: "https://github.com/username/portfolio-website",
         },
         {
             name: "Task Manager App",
             stack: ["React", "JavaScript", "CSS", "Node.js", "MongoDB"],
+            img: "el-pollo-loco.png",
             description: "Eine vollständige Task-Management-Anwendung mit Benutzerauthentifizierung und Datenbankintegration.",
             github: "https://github.com/username/task-manager-app",
         },
         {
             name: "E-Commerce Platform",
             stack: ["Vue.js", "Vuex", "Firebase", "Stripe API"],
+            img: "el-pollo-loco.png",
             description: "Eine E-Commerce-Plattform mit Produktkatalog, Warenkorb und sicherer Zahlungsabwicklung.",
             github: "https://github.com/username/ecommerce-platform",
         },
         {
             name: "Weather Forecast App",
             stack: ["Angular", "RxJS", "OpenWeatherMap API", "Chart.js"],
+            img: "el-pollo-loco.png",
             description: "Eine Wetter-App, die aktuelle Wetterdaten und Vorhersagen für verschiedene Standorte anzeigt.",
             github: "https://github.com/username/weather-forecast-app",
         },
