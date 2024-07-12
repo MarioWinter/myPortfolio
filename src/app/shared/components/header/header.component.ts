54493e6f56103e1906e0f24030372233b9f4c844
Mario Winter <mariowinter.sg@gmail.com>
2024-07-12 22:30:37 +0200

    Open and close burger menu added but without style

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BurgerMenuComponent } from "../burger-menu/burger-menu.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: "app-header",
+    standalone: true,
+    imports: [BurgerMenuComponent, CommonModule],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
+    isMenuOpen = false;
 
+    popMenu() {
+        this.isMenuOpen = !this.isMenuOpen ? true : false;
+        console.log("click geht:" + this.isMenuOpen);
+    }
+
+    showMenu() {
+        return {
+            "d-none": !this.isMenuOpen,
+        };
+    }
 }
