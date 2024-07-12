ff46886fdde4214584a20ecee842a1834d4e74d2
Mario Winter <mariowinter.sg@gmail.com>
2024-07-12 11:05:11 +0200

    Animation of the hint arrows improved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,19 +32,19 @@ export class AppComponent implements OnInit {
 				[data-aos="arrow-move-left"] {
                     transition-property: transform;
                     transform: translate3d(6.0rem, -6.0rem, 0);
-		 			animation: arrowAnimation 1s steps(6) forwards;
                 }
                 [data-aos="arrow-move-left"].aos-animate {
                     transform: translate3d(-4rem, 4rem, 0);
                 }
+
 				[data-aos="arrow-move-right"] {
                     transition-property: transform;
                     transform: translate3d(6.0rem, -6.0rem, 0);
-		 			animation: arrowAnimation 1s steps(6) forwards;
                 }
                 [data-aos="arrow-move-right"].aos-animate {
                     transform: translate3d(14rem, 0rem, 0);
                 }
+
 		`;
         this.renderer.appendChild(this.document.head, style);
     }
