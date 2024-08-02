e970e9e2c152878dc46981c5e0361e00654f115c
Mario Winter <mariowinter.sg@gmail.com>
2024-08-02 22:46:48 +0200

    Portfolio section translation customised

--- src/app/landingpage/portfolio/portfolio.component.ts
+++ src/app/landingpage/portfolio/portfolio.component.ts
@@ -2,20 +2,15 @@ import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { projectComponent } from "./project/project.component";
 import { MyprojectListService } from "../../core/services/myproject-list.service";
-import { TranslationService } from "../../core/services/translation.service";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
     selector: "app-portfolio",
     standalone: true,
-    imports: [projectComponent, CommonModule],
+    imports: [projectComponent, CommonModule, TranslateModule],
     templateUrl: "./portfolio.component.html",
     styleUrl: "./portfolio.component.scss",
 })
 export class PortfolioComponent {
     myprojectList = inject(MyprojectListService);
-    translationService = inject(TranslationService);
-
-    translate(key: string): string {
-        return this.translationService.getTranslation(this.translationService.currentLang, key);
-    }
 }
