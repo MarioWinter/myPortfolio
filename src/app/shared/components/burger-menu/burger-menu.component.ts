6ee5b367811ebdf22814bed6d548d4132cf4212a
Mario Winter <mariowinter.sg@gmail.com>
2024-08-02 23:22:29 +0200

    Menu section translation adjusted

--- src/app/shared/components/burger-menu/burger-menu.component.ts
+++ src/app/shared/components/burger-menu/burger-menu.component.ts
@@ -1,25 +1,21 @@
 import { Component, inject, EventEmitter, Output } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MyprojectListService } from "../../../core/services/myproject-list.service";
-import { TranslationService } from "../../../core/services/translation.service";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
     selector: "app-burger-menu",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, TranslateModule],
     templateUrl: "./burger-menu.component.html",
     styleUrl: "./burger-menu.component.scss",
 })
 export class BurgerMenuComponent {
     myprojectList = inject(MyprojectListService);
-    translationService = inject(TranslationService);
 
     @Output() menuToggled = new EventEmitter<void>();
 
     toggleMenu() {
         this.menuToggled.emit();
-    }
-    translate(key: string): string {
-        return this.translationService.getTranslation(this.translationService.currentLang, key);
     }
 }
