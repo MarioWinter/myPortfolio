8ff6012f6fb1729beb3c2892796156ae5f86a8c8
Mario Winter <mariowinter.sg@gmail.com>
2024-08-03 14:57:08 +0200

    Contact form styling improve for German and English

--- src/app/shared/components/burger-menu/burger-menu.component.ts
+++ src/app/shared/components/burger-menu/burger-menu.component.ts
@@ -3,18 +3,35 @@ import { CommonModule } from "@angular/common";
 import { MyprojectListService } from "../../../core/services/myproject-list.service";
 import { TranslateModule } from "@ngx-translate/core";
 
+/**
+ * @class BurgerMenuComponent
+ * @description Component for displaying a burger menu with project list and translation support.
+ * This component is standalone and imports CommonModule and TranslateModule.
+ */
 @Component({
     selector: "app-burger-menu",
     standalone: true,
     imports: [CommonModule, TranslateModule],
     templateUrl: "./burger-menu.component.html",
-    styleUrl: "./burger-menu.component.scss",
+    styleUrls: ["./burger-menu.component.scss"],
 })
 export class BurgerMenuComponent {
+    /**
+     * @property {MyprojectListService} myprojectList
+     * @description Injected service containing the list of projects.
+     */
     myprojectList = inject(MyprojectListService);
 
+    /**
+     * @output menuToggled
+     * @description Event emitter that fires when the menu is toggled.
+     */
     @Output() menuToggled = new EventEmitter<void>();
 
+    /**
+     * @method toggleMenu
+     * @description Emits an event to notify parent components that the menu has been toggled.
+     */
     toggleMenu() {
         this.menuToggled.emit();
     }
