648cbd11f7cf6fc182a8b0fe02092559d41ff240
Mario Winter <mariowinter.sg@gmail.com>
2024-08-03 14:09:58 +0200

    All arrow components Documented

--- src/app/landingpage/arrow-left/arrow-left.component.ts
+++ src/app/landingpage/arrow-left/arrow-left.component.ts
@@ -3,14 +3,25 @@ import { CommonModule } from "@angular/common";
 import { AosDirective } from "./../../shared/directives/aos.directive";
 import AOS from "aos";
 
+/**
+ * @class ArrowLeftComponent
+ * @implements {AfterViewInit}
+ * @description Component for displaying a left arrow with AOS animations.
+ * This component is standalone and imports the CommonModule and AosDirective.
+ */
 @Component({
     selector: "app-arrow-left",
     standalone: true,
     imports: [CommonModule, AosDirective],
     templateUrl: "./arrow-left.component.html",
-    styleUrl: "./arrow-left.component.scss",
+    styleUrls: ["./arrow-left.component.scss"],
 })
 export class ArrowLeftComponent implements AfterViewInit {
+    /**
+     * @method ngAfterViewInit
+     * @description Lifecycle hook that is called after a component's view has been fully initialized.
+     * It refreshes AOS to apply animations.
+     */
     ngAfterViewInit() {
         AOS.refresh();
     }
--- src/app/landingpage/arrow-right/arrow-right.component.ts
+++ src/app/landingpage/arrow-right/arrow-right.component.ts
@@ -3,14 +3,25 @@ import { CommonModule } from "@angular/common";
 import { AosDirective } from "./../../shared/directives/aos.directive";
 import AOS from "aos";
 
+/**
+ * @class ArrowRightComponent
+ * @implements {AfterViewInit}
+ * @description Component for displaying a right arrow with AOS animations.
+ * This component is standalone and imports the CommonModule and AosDirective.
+ */
 @Component({
     selector: "app-arrow-right",
     standalone: true,
     imports: [CommonModule, AosDirective],
     templateUrl: "./arrow-right.component.html",
-    styleUrl: "./arrow-right.component.scss",
+    styleUrls: ["./arrow-right.component.scss"],
 })
 export class ArrowRightComponent implements AfterViewInit {
+    /**
+     * @method ngAfterViewInit
+     * @description Lifecycle hook that is called after a component's view has been fully initialized.
+     * It refreshes AOS to apply animations.
+     */
     ngAfterViewInit() {
         AOS.refresh();
     }
