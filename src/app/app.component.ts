610b198cefec96425a6b0b6ddb683d85e198b270
Mario Winter <mariowinter.sg@gmail.com>
2024-08-07 23:47:20 +0200

    Arrow animation moved directly to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, Renderer2, Inject, AfterViewInit } from "@angular/core";
+import { Component, AfterViewInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
-import { DOCUMENT } from "@angular/common";
 
 import AOS from "aos";
 
@@ -16,8 +15,6 @@ import AOS from "aos";
 export class AppComponent implements AfterViewInit {
 	title = "myportfolio";
 
-	constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
-
 	ngAfterViewInit() {
 		setTimeout(() => {
 			AOS.init({
@@ -29,25 +26,5 @@ export class AppComponent implements AfterViewInit {
 			});
 			AOS.refresh();
 		}, 100);
-
-		const style = this.renderer.createElement("style");
-		style.textContent = `
-            [data-aos="arrow-move-left"] {
-                transition-property: transform;
-                transform: translate3d(6.0rem, -6.0rem, 0);
-            }
-            [data-aos="arrow-move-left"].aos-animate {
-                transform: translate3d(-4rem, 4rem, 0);
-            }
-
-            [data-aos="arrow-move-right"] {
-                transition-property: transform;
-                transform: translate3d(6.0rem, -6.0rem, 0);
-            }
-            [data-aos="arrow-move-right"].aos-animate {
-                transform: translate3d(14rem, 0rem, 0);
-            }
-        `;
-		this.renderer.appendChild(this.document.head, style);
 	}
 }
--- src/app/landingpage/arrow-left/arrow-left.component.ts
+++ src/app/landingpage/arrow-left/arrow-left.component.ts
@@ -1,30 +1,11 @@
-import { Component, AfterViewInit, Input } from "@angular/core";
+import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { AosDirective } from "./../../shared/directives/aos.directive";
-import AOS from "aos";
 
-/**
- * @class ArrowLeftComponent
- * @implements {AfterViewInit}
- * @description Component for displaying a left arrow with AOS animations.
- * This component is standalone and imports the CommonModule and AosDirective.
- */
 @Component({
 	selector: "app-arrow-left",
 	standalone: true,
-	imports: [CommonModule, AosDirective],
+	imports: [CommonModule],
 	templateUrl: "./arrow-left.component.html",
 	styleUrls: ["./arrow-left.component.scss"],
 })
-export class ArrowLeftComponent implements AfterViewInit {
-	@Input() id: string = "";
-
-	/**
-	 * @method ngAfterViewInit
-	 * @description Lifecycle hook that is called after a component's view has been fully initialized.
-	 * It refreshes AOS to apply animations.
-	 */
-	ngAfterViewInit() {
-		AOS.refresh();
-	}
-}
+export class ArrowLeftComponent {}
