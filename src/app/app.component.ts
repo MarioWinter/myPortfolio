3c4972f817fab207ae7216902417ac0effb1c148
Mario Winter <mariowinter.sg@gmail.com>
2024-08-07 22:49:06 +0200

    Arrow component animation improved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2, Inject } from "@angular/core";
+import { Component, Renderer2, Inject, AfterViewInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
@@ -7,45 +7,48 @@ import { DOCUMENT } from "@angular/common";
 import AOS from "aos";
 
 @Component({
-    selector: "app-root",
-    standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
-    templateUrl: "./app.component.html",
-    styleUrl: "./app.component.scss",
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent, FooterComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
-export class AppComponent implements OnInit {
-    title = "myportfolio";
+export class AppComponent implements AfterViewInit {
+	title = "myportfolio";
 
-    constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+	constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
 
-    ngOnInit() {
-        AOS.init({
-            duration: 125,
-            once: true,
-            mirror: false,
-            easing: "ease-out-cubic",
-            anchorPlacement: "center-center",
-        });
-        AOS.refresh();
-        const style = this.renderer.createElement("style");
-        style.textContent = `
-				[data-aos="arrow-move-left"] {
-                    transition-property: transform;
-                    transform: translate3d(6.0rem, -6.0rem, 0);
-                }
-                [data-aos="arrow-move-left"].aos-animate {
-                    transform: translate3d(-4rem, 4rem, 0);
-                }
+	ngAfterViewInit() {
+		// Verzögern Sie die AOS-Initialisierung leicht
+		setTimeout(() => {
+			AOS.init({
+				duration: 500,
+				once: true,
+				mirror: false,
+				easing: "ease-out-cubic",
+				anchorPlacement: "top-bottom", // Änderung hier
+			});
+			AOS.refresh();
+		}, 100);
 
-				[data-aos="arrow-move-right"] {
-                    transition-property: transform;
-                    transform: translate3d(6.0rem, -6.0rem, 0);
-                }
-                [data-aos="arrow-move-right"].aos-animate {
-                    transform: translate3d(14rem, 0rem, 0);
-                }
+		const style = this.renderer.createElement("style");
+		style.textContent = `
+            [data-aos="arrow-move-left"] {
+                transition-property: transform;
+                transform: translate3d(6.0rem, -6.0rem, 0);
+            }
+            [data-aos="arrow-move-left"].aos-animate {
+                transform: translate3d(-4rem, 4rem, 0);
+            }
 
-		`;
-        this.renderer.appendChild(this.document.head, style);
-    }
+            [data-aos="arrow-move-right"] {
+                transition-property: transform;
+                transform: translate3d(6.0rem, -6.0rem, 0);
+            }
+            [data-aos="arrow-move-right"].aos-animate {
+                transform: translate3d(14rem, 0rem, 0);
+            }
+        `;
+		this.renderer.appendChild(this.document.head, style);
+	}
 }
--- src/app/landingpage/arrow-left/arrow-left.component.ts
+++ src/app/landingpage/arrow-left/arrow-left.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from "@angular/core";
+import { Component, AfterViewInit, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AosDirective } from "./../../shared/directives/aos.directive";
 import AOS from "aos";
@@ -10,19 +10,21 @@ import AOS from "aos";
  * This component is standalone and imports the CommonModule and AosDirective.
  */
 @Component({
-    selector: "app-arrow-left",
-    standalone: true,
-    imports: [CommonModule, AosDirective],
-    templateUrl: "./arrow-left.component.html",
-    styleUrls: ["./arrow-left.component.scss"],
+	selector: "app-arrow-left",
+	standalone: true,
+	imports: [CommonModule, AosDirective],
+	templateUrl: "./arrow-left.component.html",
+	styleUrls: ["./arrow-left.component.scss"],
 })
 export class ArrowLeftComponent implements AfterViewInit {
-    /**
-     * @method ngAfterViewInit
-     * @description Lifecycle hook that is called after a component's view has been fully initialized.
-     * It refreshes AOS to apply animations.
-     */
-    ngAfterViewInit() {
-        AOS.refresh();
-    }
+	@Input() id: string = "";
+
+	/**
+	 * @method ngAfterViewInit
+	 * @description Lifecycle hook that is called after a component's view has been fully initialized.
+	 * It refreshes AOS to apply animations.
+	 */
+	ngAfterViewInit() {
+		AOS.refresh();
+	}
 }
