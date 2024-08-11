38cff544f330e22226c3709f586c4d3e35bdc4f1
Mario Winter <mariowinter.sg@gmail.com>
2024-08-11 13:35:56 +0200

    link to privacy policy added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { LandingpageComponent } from "./landingpage/landingpage.component";
 import { LegalNoticeComponent } from "./legal-notice/legal-notice.component";
+import { PrivacyPolicyComponent } from "./privacy-policy/privacy-policy.component";
 
 export const routes: Routes = [
-    { path: "", component: LandingpageComponent },
-    { path: "imprint", component: LegalNoticeComponent },
+	{ path: "", component: LandingpageComponent },
+	{ path: "imprint", component: LegalNoticeComponent },
+	{ path: "privacy", component: PrivacyPolicyComponent },
 ];
