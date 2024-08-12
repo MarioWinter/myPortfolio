e9311009fbee8d692866dc852789d183256e7c8b
Mario Winter <mariowinter.sg@gmail.com>
2024-08-13 00:10:33 +0200

    aos-animation initialised correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import AOS from "aos";
-import "aos/dist/aos.css";
 
 @Component({
 	selector: "app-root",
@@ -15,10 +14,22 @@ import "aos/dist/aos.css";
 export class AppComponent implements AfterViewInit {
 	title = "myportfolio";
 
-	ngAfterViewInit() {
-		console.log("Initializing AOS");
-		AOS.init();
+	public ngAfterViewInit() {
+		window.onload = () => {
+			this.initializeAOS();
+		};
+	}
+
+	public initializeAOS() {
+		AOS.init({
+			offset: 0,
+			duration: 500,
+			delay: 200,
+			once: true,
+			mirror: false,
+			anchorPlacement: "center-center",
+			easing: "ease-in-out",
+		});
 		AOS.refresh();
-		console.log("AOS initialized");
 	}
 }
