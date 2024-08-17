2088d4b60ca93e613a3d7bcbce4a2cf37e580d23
Mario Winter <mariowinter.sg@gmail.com>
2024-08-17 12:30:42 +0200

    AOS fade in About me img black-frame added

--- src/app/landingpage/about-me/about-me.component.ts
+++ src/app/landingpage/about-me/about-me.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
+import { AOSDirective } from "../../shared/directives/aos.directive";
 
 @Component({
-    selector: "app-about-me",
-    standalone: true,
-    imports: [TranslateModule],
-    templateUrl: "./about-me.component.html",
-    styleUrl: "./about-me.component.scss",
+	selector: "app-about-me",
+	standalone: true,
+	imports: [TranslateModule, AOSDirective],
+	templateUrl: "./about-me.component.html",
+	styleUrl: "./about-me.component.scss",
 })
 export class AboutMeComponent {}
